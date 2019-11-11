35e3a7b8f6f59a10f38bf1da7731a344cd879075
dumitru-sarov <dumitru7sarov@gmail.com>
2019-11-11 15:13:17 +0000

    added update function

--- shopLogic.sh
+++ shopLogic.sh
@@ -7,3 +7,19 @@
 # Remove
 
 # Update
+
+echo "Input item you want to update "
+read item
+
+echo "What should this be updated to? "
+read updateItem
+
+if grep -q $item items
+then
+	sed -i "s/$item/$updateItem/g" items
+	echo "Item updated"
+else 
+	echo "Item not found"
+fi
+
+
