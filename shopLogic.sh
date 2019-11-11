0d80bd414dff762bbaa686f584986a9d008f959f
msobanjo <test@test.com>
2019-11-11 15:13:25 +0000

    added search

--- shopLogic.sh
+++ shopLogic.sh
@@ -3,7 +3,16 @@
 # Add
 
 # Search
+echo Enter an item you are looking for:
+read item
 
+
+if grep -q  $item items
+then
+	echo $(grep $item items)
+else
+	echo item not found
+fi
 # Remove
 
 # Update
